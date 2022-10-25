0bcbac781402670231bee188333a4e3b74291847
Baptiste PONTANIER <baptiste.pontanier@laposte.net>
2022-10-25 17:02:05 +0200

    result feature is back, with some user validation (still needs improvement)

--- internal/slashcommands/result.go
+++ internal/slashcommands/result.go
@@ -1,7 +1,14 @@
 package slashcommands
 
 import (
+	"fmt"
+	"math"
+	"os"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
+	"github.com/haashi/omega-strikers-bot/internal/matchmaking"
+	log "github.com/sirupsen/logrus"
 )
 
 type Result struct{}
@@ -37,13 +44,21 @@ func (p Result) Options() []*discordgo.ApplicationCommandOption {
 }
 
 func (p Result) Run(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	/*match, err := match.GetByThreadId(i.ChannelID)
+	options := i.ApplicationCommandData().Options
+	optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+	for _, opt := range options {
+		optionMap[opt.Name] = opt
+	}
+	team1Score := optionMap["team1-score"].IntValue()
+	team2Score := optionMap["team2-score"].IntValue()
+
+	match, err := matchmaking.GetMatchByThreadId(i.ChannelID)
 	if err != nil {
 		log.Warningf("failed to find match by threadID %s : "+err.Error(), i.ChannelID)
 		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Content: "This channel is not a match lobby",
+				Content: "This channel is not a match lobby.",
 				Flags:   discordgo.MessageFlagsEphemeral,
 			},
 		})
@@ -52,13 +67,19 @@ func (p Result) Run(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		}
 		return
 	}
-	options := i.ApplicationCommandData().Options
-	optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
-	for _, opt := range options {
-		optionMap[opt.Name] = opt
+	if math.Abs(float64(team1Score-team2Score)) < 2 {
+		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("The result (%d-%d) is not a valid result.", team1Score, team2Score),
+			},
+		})
+		if err != nil {
+			log.Fatal("failed to send message")
+		}
+		return
 	}
-
-	var message string = fmt.Sprintf("User %s reported score : %d - %d", i.Member.Mention(), optionMap["team1-score"].IntValue(), optionMap["team2-score"].IntValue())
+	var message string = fmt.Sprintf("User %s reported score : (%d-%d).\nPlease react to this message to confirm score.", i.Member.Mention(), optionMap["team1-score"].IntValue(), optionMap["team2-score"].IntValue())
 	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
@@ -66,8 +87,46 @@ func (p Result) Run(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		},
 	})
 	if err != nil {
-		log.Fatal("failed to send message")
+		log.Error("failed to send message")
 	}
-
-	match.Close(int(optionMap["team1-score"].IntValue()), int(optionMap["team2-score"].IntValue()))*/
+	discMessage, err := s.InteractionResponse(i.Interaction)
+	if err != nil {
+		log.Error("failed to get message")
+	}
+	err = s.MessageReactionAdd(discMessage.ChannelID, discMessage.ID, "✅")
+	if err != nil {
+		log.Errorf("failed add reaction: " + err.Error())
+	}
+	err = s.MessageReactionAdd(discMessage.ChannelID, discMessage.ID, "❌")
+	if err != nil {
+		log.Errorf("failed add reaction: " + err.Error())
+	}
+	for {
+		playersOK, err := s.MessageReactions(discMessage.ChannelID, discMessage.ID, "✅", 10, "", "")
+		if err != nil {
+			log.Errorf("failed to get reactions: " + err.Error())
+		}
+		playersNOK, err := s.MessageReactions(discMessage.ChannelID, discMessage.ID, "❌", 10, "", "")
+		if err != nil {
+			log.Errorf("failed to get reactions: " + err.Error())
+		}
+		requiredReactions := 4
+		if os.Getenv("mode") == "dev" {
+			requiredReactions = 1
+		}
+		if len(playersOK) > requiredReactions {
+			err = matchmaking.CloseMatch(match, int(team1Score), int(team2Score))
+			if err != nil {
+				log.Errorf("failed to close match %s: "+err.Error(), match.ID)
+			}
+			return
+		} else if len(playersNOK) > requiredReactions {
+			err = s.ChannelMessageDelete(discMessage.ChannelID, discMessage.ID)
+			if err != nil {
+				log.Errorf("failed to delete message: " + err.Error())
+			}
+			return
+		}
+		time.Sleep(time.Second)
+	}
 }
