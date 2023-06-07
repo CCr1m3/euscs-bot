b32c9c148f345875495974deb7356fcc410ac58b
Michael Elsaesser <melsaesser@techfak.uni-bielefeld.de>
2023-06-07 18:12:25 +0200

    fixed credit-leaderboard max message index issue

--- internal/credits/leaderboard.go
+++ internal/credits/leaderboard.go
@@ -3,8 +3,10 @@ package credits
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/euscs/euscs-bot/internal/db"
 	"github.com/euscs/euscs-bot/internal/discord"
 	"github.com/euscs/euscs-bot/internal/scheduled"
@@ -23,18 +25,55 @@ func updateLeaderboard() {
 		}).Error("failed to get players ordered by credits")
 		return
 	}
-	nbMessagesNeeded := (len(players)-1)/7 + 1
+	nbMessagesNeeded := (len(players)-1)/5 + 1
 	maxMessages := 100
+
+	// count current msgs in #credits-leaderboard
+	lenMsgs := 0
 	session := discord.GetSession()
 	messages, err := session.ChannelMessages(discord.LeaderboardChannel.ID, maxMessages, "", "", "")
 	if err != nil {
-		log.WithFields(log.Fields{
-			string(static.UUIDKey):  ctx.Value(static.UUIDKey),
-			string(static.ErrorKey): err.Error(),
-		}).Error("failed to get messages from leaderboard channel")
-		return
+		if strings.Contains(err.Error(), "500 Internal Server Error") {
+			log.Info("failed to request messages" + err.Error())
+			log.Info("reattempting request...")
+			time.Sleep(time.Second / 10)
+			messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, maxMessages, "", "", "")
+			if err != nil {
+				log.Errorf("failed to reattempt request" + err.Error())
+			}
+		} else {
+			log.WithFields(log.Fields{
+				string(static.UUIDKey):  ctx.Value(static.UUIDKey),
+				string(static.ErrorKey): err.Error(),
+			}).Error("failed to get messages from leaderboard channel")
+			return
+		}
 	}
-	for i := 0; i < (nbMessagesNeeded - len(messages)); i++ {
+	for len(messages) != 0 {
+		lenMsgs += len(messages)
+		lastMessage := messages[len(messages)-1]
+		messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, maxMessages, lastMessage.ID, "", "")
+		if err != nil {
+			if strings.Contains(err.Error(), "500 Internal Server Error") {
+				log.Info("failed to request messages" + err.Error())
+				log.Info("reattempting request...")
+				time.Sleep(time.Second / 10)
+				messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, maxMessages, lastMessage.ID, "", "")
+				if err != nil {
+					log.Errorf("failed to reattempt request" + err.Error())
+				}
+			} else {
+				log.WithFields(log.Fields{
+					string(static.UUIDKey):  ctx.Value(static.UUIDKey),
+					string(static.ErrorKey): err.Error(),
+				}).Error("failed to get messages from leaderboard channel")
+				return
+			}
+		}
+	}
+
+	// send placeholder msgs if more msgs are necessary
+	for i := 0; i < (nbMessagesNeeded - lenMsgs); i++ {
 		_, err := session.ChannelMessageSend(discord.LeaderboardChannel.ID, "placeholder")
 		if err != nil {
 			log.WithFields(log.Fields{
@@ -44,8 +83,28 @@ func updateLeaderboard() {
 			return
 		}
 	}
-	for i := 0; i < (len(messages) - nbMessagesNeeded); i++ {
-		err := session.ChannelMessageDelete(discord.LeaderboardChannel.ID, messages[i].ID)
+
+	// delete unnecessary msgs
+	for i := 0; i < (lenMsgs - nbMessagesNeeded); i++ {
+		messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, 1, "", "", "")
+		if err != nil {
+			if strings.Contains(err.Error(), "500 Internal Server Error") {
+				log.Info("failed to request messages" + err.Error())
+				log.Info("reattempting request...")
+				time.Sleep(time.Second / 10)
+				messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, 1, "", "", "")
+				if err != nil {
+					log.Errorf("failed to reattempt request" + err.Error())
+				}
+			} else {
+				log.WithFields(log.Fields{
+					string(static.UUIDKey):  ctx.Value(static.UUIDKey),
+					string(static.ErrorKey): err.Error(),
+				}).Error("failed to get messages from leaderboard channel")
+				return
+			}
+		}
+		err := session.ChannelMessageDelete(discord.LeaderboardChannel.ID, messages[0].ID)
 		if err != nil {
 			log.WithFields(log.Fields{
 				string(static.UUIDKey):  ctx.Value(static.UUIDKey),
@@ -54,20 +113,63 @@ func updateLeaderboard() {
 			return
 		}
 	}
-	messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, 100, "", "", "")
+
+	// edit all current msgs
+	// get all msgs
+	messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, maxMessages, "", "", "")
 	if err != nil {
-		log.WithFields(log.Fields{
-			string(static.UUIDKey):  ctx.Value(static.UUIDKey),
-			string(static.ErrorKey): err.Error(),
-		}).Error("failed to get messages from leaderboard channel")
-		return
+		if strings.Contains(err.Error(), "500 Internal Server Error") {
+			log.Info("failed to request messages" + err.Error())
+			log.Info("reattempting request...")
+			time.Sleep(time.Second / 10)
+			messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, maxMessages, "", "", "")
+			if err != nil {
+				log.Errorf("failed to reattempt request" + err.Error())
+			}
+		} else {
+			log.WithFields(log.Fields{
+				string(static.UUIDKey):  ctx.Value(static.UUIDKey),
+				string(static.ErrorKey): err.Error(),
+			}).Error("failed to get messages from leaderboard channel")
+			return
+		}
 	}
+	var allMessages []*discordgo.Message
+	for len(messages) != 0 {
+		allMessages = append(allMessages, messages...)
+		lenMsgs += len(messages)
+		lastMessage := messages[len(messages)-1]
+		messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, maxMessages, lastMessage.ID, "", "")
+		if err != nil {
+			if strings.Contains(err.Error(), "500 Internal Server Error") {
+				log.Info("failed to request messages" + err.Error())
+				log.Info("reattempting request...")
+				time.Sleep(time.Second / 10)
+				messages, err = session.ChannelMessages(discord.LeaderboardChannel.ID, maxMessages, lastMessage.ID, "", "")
+				if err != nil {
+					log.Errorf("failed to reattempt request" + err.Error())
+				}
+			} else {
+				log.WithFields(log.Fields{
+					string(static.UUIDKey):  ctx.Value(static.UUIDKey),
+					string(static.ErrorKey): err.Error(),
+				}).Error("failed to get messages from leaderboard channel")
+				return
+			}
+		}
+	}
+	// reorder
+	var toEditMessages []*discordgo.Message
+	for n := len(allMessages) - 1; n >= 0; n-- {
+		toEditMessages = append(toEditMessages, allMessages[n])
+	}
+	// loop edit
 	contents := make([]string, nbMessagesNeeded)
 	for i, player := range players {
-		contents[i/7] += fmt.Sprintf("%d: %s (%d credits)\n", i+1, "<@"+player.DiscordID+">", player.Credits)
+		contents[i/5] += fmt.Sprintf("%d: %s (%d credits)\n", i+1, "<@"+player.DiscordID+">", player.Credits)
 	}
-	for i := 0; i < nbMessagesNeeded; i++ {
-		_, err := session.ChannelMessageEdit(discord.LeaderboardChannel.ID, messages[i].ID, contents[nbMessagesNeeded-i-1])
+	for i, msg := range toEditMessages {
+		_, err := session.ChannelMessageEdit(discord.LeaderboardChannel.ID, msg.ID, contents[i])
 		if err != nil {
 			log.WithFields(log.Fields{
 				string(static.UUIDKey):  ctx.Value(static.UUIDKey),
